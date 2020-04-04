a1e0db72dc3338c30ccd2cb33298b0b185f01c71
Juan Avendaño <juan.avendano@lhdev.es>
2020-04-04 17:07:39 +0200

    removeGitClones.bat

--- gitClones.bat
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/juamar/ZenvaWishlist.git
-git clone https://github.com/juamar/zenvaVideoWidget.git
-git clone https://github.com/juamar/ZenvaTitle.git
-git clone https://github.com/juamar/ZenvaShortcodes.git
-git clone https://github.com/juamar/ZenvaRelatedPosts.git
-git clone https://github.com/juamar/ZenvaCron.git
